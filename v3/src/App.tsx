a50d096bcf548cc77e248a1cf0cd2fa0a490726f
bluelion2 <iltk2000@gmail.com>
2019-06-27 15:10:41 +0900

    init: v3 init & create(TS)

---
+++ v3/src/App.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import './App.css';
+import { TodoItem } from './component/todoItem';
+import AddTodo from './component/addTodo';
+
+interface Props {}
+
+export interface Todo {
+  id: number;
+  complete: boolean;
+  content: string;
+}
+
+export default class App extends React.Component<Props> {
+  state: { todoList: Array<Todo> };
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      todoList: [
+        { id: 1, content: 'HTML', complete: false },
+        { id: 2, content: 'CSS', complete: false },
+        { id: 3, content: 'JS', complete: true },
+      ]
+    }
+  }
+
+  handleSubmit = (content: string): void => {
+    const item = { id: this.createId(), content, complete: false };
+    this.setState({ 
+      todoList: this.state.todoList.concat(item),
+    });
+  }
+
+  createId = (): number => {
+    const { todoList } = this.state;
+    return todoList.length ? Math.max(...todoList.map(list => list.id)) + 1 : 1
+  }
+
+  handleChange = (todo: Todo): void => {
+    const { todoList } = this.state;
+    todoList.forEach(item => {
+      if (todo.id === item.id) {
+        item.complete = !item.complete
+      };
+    });
+    this.setState({ todoList })
+  }
+
+  handleRemove = (id: number): void => {
+    const todoList = this.state.todoList.filter(todo => todo.id !== id);
+    this.setState({ todoList });
+  }
+
+  render(): JSX.Element {
+    const { todoList } = this.state;
+    
+    return (
+      <main>
+        <h2>Todo List</h2>
+        <div className="container">
+          <AddTodo onSubmit={ this.handleSubmit }/>
+          <ul>
+            { 
+              todoList.map(todo => {
+                return (
+                  <TodoItem
+                    todo={ todo }
+                    key={ todo.id }
+                    handleChange={ this.handleChange }
+                    handleRemove={ this.handleRemove }
+                  />
+                )
+              })
+            }
+          </ul>
+        </div>
+      </main>
+    )
+  }
+}
